165fd97cffb7c94c21fa141a3d3e75bcc1873d1f
Corbin Wunderlich <corbin.wunderlich@gmail.com>
2024-09-28 19:20:05 -0400

    Add zsh-vi-mode ZSH plugin

--- users/corbin/tools/zsh.nix
+++ users/corbin/tools/zsh.nix
@@ -8,7 +8,11 @@ in {
   options.zsh.enable = lib.mkEnableOption "Enables ZSH";
 
   config = lib.mkIf config.zsh.enable {
-    home.packages = with pkgs; [ zsh-autocomplete zsh-autosuggestions ];
+    home.packages = with pkgs; [
+      zsh-autocomplete
+      zsh-autosuggestions
+      zsh-vi-mode
+    ];
 
     programs.zsh = {
       enable = true;
@@ -45,6 +49,15 @@ in {
             repo = "zsh-autosuggestions";
             rev = "0.7.0";
             sha256 = "sha256-o8IQszQ4/PLX1FlUvJpowR2Tev59N8lI20VymZ+Hp4w=";
+          };
+        }
+        {
+          name = "zsh-vi-mode";
+          src = pkgs.fetchFromGitHub {
+            owner = "jeffreytse";
+            repo = "zsh-vi-mode";
+            rev = "v0.11.0";
+            sha256 = "sha256-xbchXJTFWeABTwq6h4KWLh+EvydDrDzcY9AQVK65RS8=";
           };
         }
       ];
